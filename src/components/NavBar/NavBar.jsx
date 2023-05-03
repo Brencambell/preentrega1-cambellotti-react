df0ad1f9d9e84249953bb9749416d6ba08cb7f6b
Brenda Cambellotti <brencambellotti@gmail.com>
2023-05-03 20:17:48 -0300

    creando NavBar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Craft Beer E-commerce</h3>
+            <div>
+                <button>IPA's</button>
+                <button>Lagers</button>
+                <button>Sours</button>
+                <button>Darks</button>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
