037bb2588b6121304f2e349b1201c478c14e2b58
Brenda Cambellotti <brencambellotti@gmail.com>
2023-05-03 20:30:42 -0300

    Creando CartWidget

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,13 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import { BiCart } from 'react-icons/bi';
+
+const CartWidget = () => {
+    return (
+        <div>
+            <BiCart className="bi bi-cart" />
+            0
+         </div>
+    )
+}
+
+export default CartWidget
