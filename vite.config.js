9f9251fdbebe0d45df88dc278e9d9b9dd742c6d0
Brenda Cambellotti <brencambellotti@gmail.com>
2023-05-04 16:20:36 -0300

    base update para que funcione github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://Brencambell.github.io/preentrega1-cambellotti-react'
 })
